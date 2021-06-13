ebfd3907b6dac264a79d083f6445728cf985139d
DELL <vicmar.yanson@gmail.com>
2021-06-14 00:59:11 +0800

    Initial Commit

---
+++ assets/custom.js
@@ -0,0 +1,6 @@
+$(function () {
+  $(".tmenu_item--root:nth-child(7)>.tmenu_item_link").attr("href", "https://homecraft-textiles-wa-pty-ltd.myshopify.com/pages/apparel-fashion-fabrics");
+  
+  $(".announcement-bar .annoubar-column>div.annoubar-colItem-text:nth-child(1) p").before("<img src='https://cdn.shopify.com/s/files/1/0538/6810/1801/files/164192404_2624744607822852_7237634618593247146_n.png?v=1616858892'>");
+  $(".announcement-bar .annoubar-column>div.annoubar-colItem-text:nth-child(2) p").before("<img src='https://cdn.shopify.com/s/files/1/0538/6810/1801/files/164828427_779087719405891_6146262452956795093_n.png?v=1616858900'>")
+});
